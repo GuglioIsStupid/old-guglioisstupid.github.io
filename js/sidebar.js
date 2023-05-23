3a29e8b0a6bbd79d7c98cf3fd8025083d959e0d7
GuglioIsStupid <bradsbluetab@gmail.com>
2023-05-23 15:37:31 -0400

    upload more stuff

---
+++ js/sidebar.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("myNav").style.width = "60%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+console.log("sidebar.js loaded");
