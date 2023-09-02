3f56634cf553eed372688bd922fa37e14f340a28
GuglioIsStupid <bradsbluetab@gmail.com>
2023-09-01 23:47:40 -0400

    Move css and js to seperate files

---
+++ index.js
@@ -0,0 +1,20 @@
+function copyDiscord() {
+    // Set "guglioisstupid" to the clipboard
+    navigator.clipboard.writeText("guglioisstupid");
+    // change text to "Copied!" , then back to "Discord"
+    document.getElementById("discord").innerHTML = "<div class=\"discordInfo\"><h3>Copied!</h3><p>guglioisstupid</p><button onclick=\"copyDiscord()\">Copy</button></div>";
+    setTimeout(function() {
+        document.getElementById("discord").innerHTML = "<div class=\"discordInfo\"><h3>Discord</h3><p>guglioisstupid</p><button onclick=\"copyDiscord()\">Copy</button></div>";
+
+    }, 1000);
+}
+
+function GoToTwitter() {
+    // open twitter in new tab
+    window.open("https://twitter.com/GuglioIs2Stupid");
+}
+
+function GoToGithub() {
+    // open github in new tab
+    window.open("https://github.com/GuglioIsStupid");
+}
